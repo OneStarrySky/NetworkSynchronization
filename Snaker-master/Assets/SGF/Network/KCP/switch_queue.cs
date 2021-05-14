08171d42efc0265810924970f5d9c8e65e6f6e27
OneStarrySky <84018311+OneStarrySky@users.noreply.github.com>
2021-05-14 15:49:21 +0800

    帧同步
    贪吃蛇小游戏

---
+++ Snaker-master/Assets/SGF/Network/KCP/switch_queue.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+
+namespace SGF.Network.KCP
+{
+    public class Utility
+    {
+        /// <summary>
+        /// 值类型数据交换
+        /// </summary>
+        /// <typeparam name="QT"></typeparam>
+        /// <param name="t1"></param>
+        /// <param name="t2"></param>
+        public static void Swap<QT>(ref QT t1, ref QT t2)
+        {
+
+            QT temp = t1;
+            t1 = t2;
+            t2 = temp;
+        }
+    }
+
+    public class SwitchQueue<T> where T : class
+    {
+
+        private Queue mConsumeQueue;
+        private Queue mProduceQueue;
+
+        public SwitchQueue()
+        {
+            mConsumeQueue = new Queue(16);
+            mProduceQueue = new Queue(16);
+        }
+
+        public SwitchQueue(int capcity)
+        {
+            mConsumeQueue = new Queue(capcity);
+            mProduceQueue = new Queue(capcity);
+        }
+
+        // producer
+        public void Push(T obj)
+        {
+            lock (mProduceQueue)
+            {
+                mProduceQueue.Enqueue(obj);
+            }
+        }
+
+        // consumer.
+        public T Pop()
+        {
+
+            return (T)mConsumeQueue.Dequeue();
+        }
+
+        public bool Empty()
+        {
+            return 0 == mConsumeQueue.Count;
+        }
+
+        public void Switch()
+        {
+            lock (mProduceQueue)
+            {
+                Utility.Swap(ref mConsumeQueue, ref mProduceQueue);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (mProduceQueue)
+            {
+                mConsumeQueue.Clear();
+                mProduceQueue.Clear();
+            }
+        }
+    }
+
+
+}
